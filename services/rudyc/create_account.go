dccffbb904ebf6f450948bb6faf1f9620be10894
Marlon Henry Schweigert <fleyhe0@gmail.com>
2018-11-22 00:44:15 -0200

    Create account

---
+++ services/rudyc/create_account.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/parnurzeal/gorequest"
+	"github.com/schweigert/mga/model"
+)
+
+type createAccountRequestEnvelope struct {
+	Body string `json:"body"`
+}
+
+type createAccountRequestSubEnvelope struct {
+	Account *model.Account `json:"Value"`
+}
+
+func createAccountRequest() (gorequest.Response, string, []error) {
+	return gorequest.New().Post(RUDYWEB_ACCOUNT_PATH).Send(USER_ACCOUNT).End()
+}
+
+func createAccount() {
+	resp, body, errs := createAccountRequest()
+	if len(errs) != 0 {
+		panic(errs[0])
+	}
+
+	if resp.StatusCode != 200 {
+		panic("Status Code != 200 on create account")
+	}
+
+	data := &createAccountRequestEnvelope{}
+	if err := json.Unmarshal([]byte(body), data); err != nil {
+		panic(err)
+	}
+
+	log.Println(data.Body)
+
+	value := &createAccountRequestSubEnvelope{Account: &USER_ACCOUNT}
+	if err := json.Unmarshal([]byte(data.Body), &value); err != nil {
+		panic(err)
+	}
+}
--- services/rudyc/main.go
+++ services/rudyc/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"log"
 	"math/rand"
+	"os"
 	"strconv"
 
 	"github.com/schweigert/mga/libraries/security"
 	"github.com/schweigert/mga/model"
 )
 
-var USER_ACCOUNT model.Account
+var (
+	USER_ACCOUNT           model.Account
+	RUDYWEB_URL            string
+	RUDYWEB_ACCOUNT_PATH   string
+	RUDYWEB_CHARACTER_PATH string
+)
 
 func initUsername() {
 	USER_ACCOUNT.Username = "user#" + strconv.Itoa(rand.Int()%100000)
@@ -20,13 +26,24 @@ func initPassword() {
 	USER_ACCOUNT.Password = security.Hash(unsecuredPassword)
 }
 
+func initRudywebUrl() {
+	RUDYWEB_URL = os.Getenv("RUDYWEB_URL")
+	RUDYWEB_ACCOUNT_PATH = RUDYWEB_URL + "/account/create"
+	RUDYWEB_CHARACTER_PATH = RUDYWEB_URL + "/character/create"
+}
+
 func init() {
 	initUsername()
 	initPassword()
+	initRudywebUrl()
 }
 
 func main() {
 	log.Println("Starting Rudy Client...")
 	log.Println("Username:", USER_ACCOUNT.Username)
 	log.Println("Password:", USER_ACCOUNT.Password)
+
+	log.Println("Rudyweb URL:", RUDYWEB_URL)
+
+	createAccount()
 }
