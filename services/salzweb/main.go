dfe38dff474a02c1f72e48c34599bfb6999beacf
Marlon Henry Schweigert <marlon.henry@magrathealabs.com>
2019-01-22 20:45:29 -0200

    Main work arround

--- services/salzweb/main.go
+++ services/salzweb/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/schweigert/mga/libraries/db"
+	"github.com/schweigert/mga/model"
 )
 
 var router *gin.Engine
@@ -13,6 +15,17 @@ func init() {
 	router = gin.Default()
 }
 
+func crateAccountHandler(c *gin.Context) {
+	dbc := db.Connect()
+	defer db.SafeClose(dbc)
+
+	account := &model.Account{}
+	if c.BindJSON(account) == nil {
+		dbc.Create(account)
+	}
+}
+
 func main() {
+	router.POST("accounts/", crateAccountHandler)
 	log.Panic(router.Run(os.Getenv("ADDR")))
 }
